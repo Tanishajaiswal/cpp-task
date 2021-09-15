78a766c504d92a2c943d946e42cf673b04e1e722
Tanisha <86921810+Tanishajaiswal@users.noreply.github.com>
2021-09-15 20:11:03 +0530

    Create task1.c

---
+++ task1.c
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   cout<<"Size of int is "<<sizeof(int)<<" bytes\n"<<flush;
+   cout<<"Size of float is "<<sizeof(float)<<" bytes\n"<<flush;
+   cout<<"Size of double is "<<sizeof(double)<<" bytes\n"<<flush;
+   cout<<"Size of char is "<<sizeof(char)<<" byte\n"<<flush;
+   return 0;
+}

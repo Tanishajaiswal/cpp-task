c22f6f70ee7c254c98683b618267f32432c44b39
Tanisha <86921810+Tanishajaiswal@users.noreply.github.com>
2021-09-15 20:18:57 +0530

    Create task2.c

---
+++ task2.c
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    double n1, n2, product;
+    cout<<"enter two numbers :";
+    cin>> n1>>n2;
+    product = n1*n2;
+    cout<< "product =" <<product;
+    return 0;
+}
